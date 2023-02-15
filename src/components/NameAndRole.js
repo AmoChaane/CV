98787a843977ade91a32fe76e2cde5d485a822fe
Amogelang Chaane <amogelangchaane200@gmail.com>
2023-02-15 23:37:08 +0200

    added more style along the role text

--- src/components/NameAndRole.js
+++ src/components/NameAndRole.js
@@ -9,7 +9,10 @@ export default class NameAndRole extends React.Component {
         return <div className="NameAndRole">
             <h1 className="firstName">AMOGELANG</h1>
             <h1 className="lastName">CHAANE</h1>
-            <h4 className="role">WEB DEVELOPER</h4>
+            <div>
+                <div></div>
+                <h4 className="role">WEB DEVELOPER</h4>
+            </div>
         </div>
     }
 }
